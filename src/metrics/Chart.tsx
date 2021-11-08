9a5890287a40d4a47b081f1846ade61425e12702
Sfimadi94 <Sydeffect1994@gmail.com>
2021-11-08 15:49:57 -0600

    chart is now working

--- src/metrics/Chart.tsx
+++ src/metrics/Chart.tsx
@@ -31,17 +31,37 @@ const useStyles = makeStyles({
 const updateData = (selectedData: any, newMeasurement: any) => {
   let newArray: any[] = [];
   const newSelectedData = [...selectedData];
+  // console.log(newSelectedData, 'first', newMeasurement.newMeasurement.metric);
   for (let i = 0; i < selectedData.length; i += 1) {
+    // if (
+    //   selectedData[i].measurements[0].metric.includes(
+    //     newMeasurement.newMeasurement.metric,
+    //   )
+    // ) {
+    //   newArray = [...selectedData[i].measurements];
+    //   newArray.push(newMeasurement.newMeasurement);
+    //   newArray.shift();
+
+    //   const newDataTest = { ...newSelectedData[i] };
+    //   // newDataTest = newArray;
+    //   // newSelectedData[i]= newArray;
+    //   newDataTest.measurements = newArray;
+    //   newSelectedData[i] = newDataTest;
+    // }
+    // prettier-ignore
     if (
-      selectedData[i].measurements[0].metric.includes(
-        newMeasurement.newMeasurement.metric,
-      )
+      selectedData[i].measurements[0].metric
+      === newMeasurement.newMeasurement.metric
     ) {
       newArray = [...selectedData[i].measurements];
       newArray.push(newMeasurement.newMeasurement);
       newArray.shift();
 
-      newSelectedData[i] = newArray;
+      const newDataTest = { ...newSelectedData[i] };
+      // newDataTest = newArray;
+      // newSelectedData[i]= newArray;
+      newDataTest.measurements = newArray;
+      newSelectedData[i] = newDataTest;
     }
   }
 
@@ -52,7 +72,7 @@ function Chart() {
   const classes = useStyles();
   const dispatch = useDispatch();
   const [initialData, setInitialData] = React.useState<Measurement[]>([]);
-  // const [newData, setNewData] = React.useState<Measurement[]>([])
+  // const [newData, setNewData] = React.useState<Measurement[]>([]);
 
   // prettier-ignore
   const {
@@ -74,38 +94,49 @@ function Chart() {
   const newValue = useSubscription(NEW_MEASUREMENT_SUBSCRIPTION);
 
   useEffect(() => {
-    const fetchData = () => {
+    const fetchData = async () => {
       if (data) {
-        const { getMultipleMeasurements } = data;
+        const { getMultipleMeasurements } = await data;
         dispatch(actions.selectMultipleMetricsList(getMultipleMeasurements));
-        setInitialData(data);
+        setInitialData(getMultipleMeasurements);
+
+        console.log(initialData, 'one');
+      }
+      if (error) {
+        console.log('error');
       }
     };
 
     fetchData();
-  }, [dispatch, data, error, initialData]);
+  }, [dispatch, data]);
 
+  // [dispatch, data, error, setInitialData]
   useEffect(() => {
-    const { loading } = newValue;
+    const getData = async () => {
+      const { loading } = await newValue;
 
-    if (
-      // prettier-ignore
-      Object.keys(subscription).length > 0
+      if (
+        // prettier-ignore
+        Object.keys(subscription).length > 0
       && Object.keys(selectedMetricsList).length > 0
-    ) {
-      // This updates the old value with the new subscription values
+      ) {
+        // This updates the old value with the new subscription values
+        const freshData = updateData(selectedMetricsList, subscription);
+        // console.log(freshData, 'fresh');
+        dispatch(actions.getFreshData(freshData));
+        dispatch(actions.selectMultipleMetricsList(freshData));
+        setInitialData(freshData);
+        console.log(initialData, 'two');
+        // setInitialData(freshData);
+      }
 
-      const freshData = updateData(selectedMetricsList, subscription);
-      dispatch(actions.getFreshData(freshData));
-      // dispatch(actions.selectMultipleMetricsList(freshData));
-      setInitialData(freshData);
-      const test = selectedMetricsList.map((item) => item);
-    }
+      if (!loading) {
+        dispatch(actions.getSubscriptions(newValue.data));
+      }
+    };
 
-    if (!loading) {
-      dispatch(actions.getSubscriptions(newValue.data));
-    }
-  }, [newValue.data]);
+    getData();
+  }, [newValue.data, dispatch]);
 
   if (!data) {
     return <LinearProgress />;
